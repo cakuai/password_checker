c85d0175e7d9705501aa39e42e3f3bca4aa58768
cakuai <angel09931935@gmail.com>
2020-10-13 23:38:52 +0800

    first upload, functions are still pending

---
+++ pw_checker.py
@@ -0,0 +1,23 @@
+import requests  # 建立各種 HTTP 請求，從網頁伺服器上取得想要的資料。
+import sys
+import hashlib
+
+
+def request_api_data(query_char):
+    url = "https://api.pwnedpasswords.com/range/" + query_char
+    # data fr. https://haveibeenpwned.com/API/v3
+    # 此ＡＰＩ的功能可回傳所有以 該5字 開頭的password及被leak的次數
+    res = requests.get(url)
+    if res.status_code != 200:  # 對該ＡＰＩ而言 200才代表正常運行
+        raise RuntimeError(
+            f"Here's the Error: {res.status_code}, check the API and try again")
+    return res
+
+
+def pwned_api_check(password):
+    # check whether or not the password exists in API response
+    sha1password = hashlib.sha1(password.encode("UTF-8")).hexdigest
+    return sha1password
+
+
+print(pwned_api_check("123"))
